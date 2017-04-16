8b262027556f4373d958d64b20a28ba428cb1e13
Yuhei Yasuda <yuhei.yasuda1003@gmail.com>
2017-04-17 01:15:58 +0900

    refactor

--- src/js/polyfills.js
+++ src/js/polyfills.js
@@ -1,7 +1,7 @@
 import 'es6-promise/auto'
 
 export default async () => {
-  if (!self.fetch) {
+  if (!window.fetch) {
     await import('whatwg-fetch')
   }
 }
--- src/js/util/user-agent.js
+++ src/js/util/user-agent.js
@@ -2,18 +2,16 @@ const ua = navigator.userAgent
 
 let _os
 export const detectOs = () => {
-  if (_os) {
-    return _os
-  }
-
-  if (ua.match(/Windows NT/)) {
-    _os = 'Windows'
-  } else if (ua.match(/Mac OS X/)) {
-    _os = 'Mac OS'
-  } else if (ua.match(/iPhone OS/) || ua.match(/iPad; CPU OS/)) {
-    _os = 'iOS'
-  } else {
-    _os = 'Unknown'
+  if (!_os) {
+    if (ua.match(/Windows NT/)) {
+      _os = 'Windows'
+    } else if (ua.match(/Mac OS X/)) {
+      _os = 'Mac OS'
+    } else if (ua.match(/iPhone OS/) || ua.match(/iPad; CPU OS/)) {
+      _os = 'iOS'
+    } else {
+      _os = 'Unknown'
+    }
   }
 
   return _os
