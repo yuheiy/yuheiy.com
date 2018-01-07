bbb01d93d33f06146dd7a3b149a0fbbe64765b51
Yuhei Yasuda <yuhei.yasuda1003@gmail.com>
2018-01-07 23:06:17 +0900

    use defer for nomodule

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,11 @@
 import 'what-input'
 import './yuhei-avator.js'
-import { parsed as documentParsed } from 'document-promises'
 import webComponentsFallback from './webcomponents-fallback.js'
 import PageFooter from './PageFooter.js'
 
 document.documentElement.classList.remove('no-js')
-;(async () => {
-    await documentParsed
-    webComponentsFallback()
-    PageFooter()
-})()
+
+webComponentsFallback()
+PageFooter()
 
 console.log('Source: https://github.com/yuheiy/yuheiy.com')
