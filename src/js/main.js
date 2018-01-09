1fd755c8636c7f519cf57391558b9a0536dd8215
Yuhei Yasuda <yuhei.yasuda1003@gmail.com>
2018-01-09 18:21:29 +0900

    rename function names

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,11 @@
 import 'what-input'
 import './yuhei-avator.js'
-import webComponentsFallback from './webcomponents-fallback.js'
-import playYuheiAvator from './play-yuhei-avator.js'
+import fallbackWebComponents from './webcomponents-fallback.js'
+import initFooter from './footer.js'
 
 document.documentElement.classList.remove('no-js')
 
-webComponentsFallback()
-playYuheiAvator()
+fallbackWebComponents()
+initFooter()
 
 console.log('Source: https://github.com/yuheiy/yuheiy.com')
