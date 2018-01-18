4af1926e6a56a11ed621e76f0435e3ae8048691f
Yuhei Yasuda <yuhei.yasuda1003@gmail.com>
2018-01-18 23:42:33 +0900

    flatten some scripts

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,47 @@
 import 'what-input'
 import './yuhei-avator.js'
-import fallbackWebComponents from './webcomponents-fallback.js'
-import initFooter from './footer.js'
+import throttle from 'lodash-es/throttle'
+import { hasTouch, canUseWebComponents, shouldReduceMotion } from './utils.js'
 
 document.documentElement.classList.remove('no-js')
 
-fallbackWebComponents()
-initFooter()
+if (canUseWebComponents && !shouldReduceMotion) {
+    const anchorEl = document.querySelector('body > footer a[title="yuheiy.com"]')
+    const avatorEl = anchorEl.querySelector('yuhei-avator')
+
+    const checkActivity = () => {
+        const isHovered = !hasTouch && anchorEl.matches(':hover')
+        const isKeyboardFocused = anchorEl.matches('html[data-whatinput="keyboard"] :focus')
+        const shouldPlay = isHovered || isKeyboardFocused
+
+        if (shouldPlay) {
+            avatorEl.play()
+        } else {
+            avatorEl.stop()
+        }
+    }
+
+    // mouse
+    document.addEventListener('blur', checkActivity)
+    document.addEventListener('mousemove', throttle(checkActivity, 1000 / 5))
+    anchorEl.addEventListener('mouseenter', checkActivity)
+    anchorEl.addEventListener('mouseleave', checkActivity)
+
+    // keyboard
+    anchorEl.addEventListener('focus', checkActivity)
+    anchorEl.addEventListener('blur', checkActivity)
+}
+
+if (!canUseWebComponents) {
+    const tmplEl = document.querySelector('#tmpl-yuhei-avator-fallback')
+
+    document.querySelectorAll('yuhei-avator').forEach((originalEl) => {
+        const replacementEl = document.importNode(tmplEl.content.firstElementChild, true)
+        ;[...originalEl.attributes].forEach(({ name, value }) => {
+            replacementEl.setAttribute(name, value)
+        })
+        originalEl.replaceWith(replacementEl)
+    })
+}
 
 console.log('Source: https://github.com/yuheiy/yuheiy.com')
