337fcc20d09338b060c8e008ecb86a9d66f68e89
Yuhei Yasuda <yuhei.yasuda1003@gmail.com>
2018-07-14 18:49:33 +0900

    simplify build tasks

--- bin.js
+++ bin.js
@@ -1,105 +1,91 @@
-const path = require('path')
-const fs = require('fs')
-const { promisify } = require('util')
-const { JSDOM } = require('jsdom')
-const browserSync = require('browser-sync')
-const renderHelper = require('real-world-website-render-helper')
-const pug = require('pug')
-const sass = require('node-sass')
-const red = require('ansi-red')
-const postcss = require('postcss')
+const del = require('del')
+const vfs = require('vinyl-fs')
+const rename = require('gulp-rename')
+const gulpif = require('gulp-if')
+const sourcemaps = require('gulp-sourcemaps')
+const sass = require('gulp-sass')
+const globImporter = require('node-sass-glob-importer')
+const postcss = require('gulp-postcss')
 const autoprefixer = require('autoprefixer')
-const gapProperties = require('postcss-gap-properties')
 const csswring = require('csswring')
 const { rollup } = require('rollup')
-const del = require('del')
-const chokidar = require('chokidar')
-const cpx = require('cpx')
+const resolve = require('rollup-plugin-node-resolve')
+const commonjs = require('rollup-plugin-commonjs')
+const { uglify } = require('rollup-plugin-uglify')
+const { minify } = require('uglify-es')
+const { JSDOM } = require('jsdom')
+const pug = require('pug')
+const renderHelper = require('real-world-website-render-helper')
+const browserSync = require('browser-sync')
 const { series, parallel } = require('bach')
-const rollupConfig = require('./rollup.config')
 
-const isProd = process.argv[2] === '--prod'
+let isDebug = true
 
-let blogPosts = null
+const clean = () => {
+  return del('dist')
+}
 
-const loadBlogPosts = async () => {
-  if (!blogPosts) {
-    const {
-      window: { document },
-    } = await JSDOM.fromURL('https://yuheiy.hatenablog.com/feed')
-    const posts = [...document.querySelectorAll('entry')].map((entryEl) => {
-      const title = entryEl.querySelector('title').textContent
-      const url = entryEl.querySelector('link').getAttribute('href')
-      return { title, url }
-    })
-    blogPosts = posts
+let sassImporters = null
+let postcssPlugins = null
+
+const css = () => {
+  if (!sassImporters) {
+    sassImporters = [globImporter()]
   }
 
-  return blogPosts
-}
-
-const renderHelperConfig = {
-  input: 'src/html',
-  inputExt: 'pug',
-  output: 'dist',
-  outputExt: 'html',
-  render: async ({ src, filename }) => {
-    return pug.render(src.toString(), {
-      blogPosts: await loadBlogPosts(),
-      filename,
-      basedir: 'src/html',
-    })
-  },
-}
-
-const writeFileAsync = promisify(fs.writeFile)
-
-const css = async () => {
-  let sassResult
-  try {
-    sassResult = await new Promise((resolve, reject) => {
-      sass.render(
-        {
-          file: 'src/css/main.scss',
-          outFile: 'src/css/main.css',
-          sourceMap: !isProd,
-          sourceMapContents: true,
-        },
-        (err, result) => {
-          if (err) {
-            reject(err)
-            return
-          }
-          resolve(result)
-        },
-      )
-    })
-  } catch (err) {
-    const filePath = path.relative(__dirname, err.file)
-    console.log(red(`Error in ${filePath}`))
-    console.log(err.formatted.toString())
-    return
+  if (!postcssPlugins) {
+    postcssPlugins = [
+      autoprefixer({
+        cascade: false,
+      }),
+      !isDebug && csswring(),
+    ].filter(Boolean)
   }
 
-  const postcssResult = await postcss([
-    gapProperties(),
-    autoprefixer({
-      cascade: false,
-    }),
-    ...(isProd ? [csswring()] : []),
-  ]).process(sassResult.css, {
-    from: 'main.css',
-    to: 'main.css',
-    map: !isProd && {
-      inline: true,
-      prev: JSON.parse(sassResult.map),
-    },
-  })
-
-  await writeFileAsync('src/html/main.css', postcssResult.css)
+  return vfs
+    .src('src/main.scss')
+    .pipe(rename({ suffix: '.bundle' }))
+    .pipe(gulpif(isDebug, sourcemaps.init()))
+    .pipe(sass({ importer: sassImporters }).on('error', sass.logError))
+    .pipe(postcss(postcssPlugins))
+    .pipe(gulpif(isDebug, sourcemaps.write()))
+    .pipe(vfs.dest('src'))
 }
+
+let rollupConfig = null
 
 const js = async () => {
+  if (!rollupConfig) {
+    rollupConfig = [
+      {
+        input: './src/main.mjs',
+        plugins: [
+          resolve(),
+          commonjs(),
+          !isDebug &&
+            uglify(
+              {
+                output: {
+                  comments: (_, { value, type }) => {
+                    if (type === 'comment2') {
+                      // multiline comment
+                      return /@preserve|@license|@cc_on/i.test(value)
+                    }
+                  },
+                },
+              },
+              minify,
+            ),
+        ].filter(Boolean),
+      },
+      {
+        file: './src/main.bundle.mjs',
+        format: 'es',
+        sourcemap: isDebug && 'inline',
+      },
+    ]
+  }
+
   try {
     const { write } = await rollup(rollupConfig[0])
     return write(rollupConfig[1])
@@ -108,15 +94,73 @@ const js = async () => {
   }
 }
 
-const bs = browserSync.create()
+let blogPosts = null
+
+const loadBlogPosts = async () => {
+  if (!blogPosts) {
+    const dom = await JSDOM.fromURL('https://yuheiy.hatenablog.com/feed')
+    const posts = [...dom.window.document.querySelectorAll('entry')].map(
+      (entryEl) => {
+        const title = entryEl.querySelector('title').textContent
+        const url = entryEl.querySelector('link').getAttribute('href')
+        return { title, url }
+      },
+    )
+    blogPosts = posts
+  }
+
+  return blogPosts
+}
+
+const htmlRendererConfig = {
+  input: 'src',
+  inputExt: 'pug',
+  output: 'dist',
+  outputExt: 'html',
+  render: async ({ src, filename }) => {
+    return pug.render(String(src), {
+      blogPosts: await loadBlogPosts(),
+      filename,
+      basedir: 'src',
+    })
+  },
+}
+
+const html = () => {
+  return renderHelper.build(htmlRendererConfig)
+}
+
+const copy = () => {
+  return vfs.src('public/**/*', { dot: true }).pipe(vfs.dest('dist'))
+}
 
 const serve = (done) => {
+  const bs = browserSync.create()
+  const renderHtmlMiddleware = renderHelper.createRenderMiddleware(
+    htmlRendererConfig,
+  )
+
   bs.init(
     {
       notify: false,
       ui: false,
       server: ['dist', 'public'],
-      middleware: renderHelper.createRenderMiddleware(renderHelperConfig),
+      files: [
+        {
+          match: ['src/**/*.pug', 'src/**/*.bundle.*'],
+          fn: bs.reload,
+        },
+        {
+          match: 'src/**/*.scss',
+          fn: css,
+        },
+        {
+          match: ['src/**/*.mjs', '!src/**/*.bundle.mjs'],
+          fn: js,
+        },
+      ],
+      watchEvents: ['add', 'change', 'unlink'],
+      middleware: renderHtmlMiddleware,
       ghostMode: false,
       open: false,
     },
@@ -124,32 +168,26 @@ const serve = (done) => {
   )
 }
 
-const clean = () => {
-  return del('dist')
+const script = process.argv[2]
+let main = null
+
+switch (script) {
+  case 'start': {
+    main = series(clean, parallel(css, js), serve)
+    break
+  }
+  case 'build': {
+    isDebug = false
+    main = series(clean, parallel(series(parallel(css, js), html), copy))
+    break
+  }
 }
 
-const watch = (done) => {
-  const opts = { ignoreInitial: true }
-  chokidar.watch('src/css/**/*.scss', opts).on('all', css)
-  chokidar.watch('src/js/**/*.js', opts).on('all', js)
-  chokidar.watch(['src/html/**/*', 'public/**/*'], opts).on('all', bs.reload)
-  done()
-}
-
-const html = () => {
-  return renderHelper.build(renderHelperConfig)
-}
-
-const copy = () => {
-  return Promise.all(
-    ['public/**/*', 'public/.nojekyll'].map(
-      (src) => new Promise((resolve) => cpx.copy(src, 'dist', resolve)),
-    ),
-  )
-}
-
-if (isProd) {
-  series(clean, parallel(series(parallel(css, js), html), copy))()
-} else {
-  series(clean, parallel(css, js), serve, watch)()
+if (main) {
+  main((err) => {
+    if (err) {
+      process.exitCode = 1
+      console.trace(err)
+    }
+  })
 }
